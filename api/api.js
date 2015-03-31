0269b92300c8b7f6b385b43d5a0099395846e6b8
André Gomes Lamas Otero <lamas3000@gmail.com>
2015-03-31 12:29:59 -0300

    adicionado os demais pacotes usados na API atualizados o README.md da API com a convenção de data

--- api/api.js
+++ api/api.js
@@ -7,8 +7,12 @@ var express = require('express');
 //criamos a instancia do App
 var app = express();
 
+//criamos instancia do body-parser, usado nos handlers
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
+
+//criamos instancia do moment para tratar datas
+var moment = require('moment');
 
 //precisamos depois mudar a constante para uma gerada ao logar!
 var tokenFake = "ASKDJHQWOEY98172354123";
@@ -96,10 +100,60 @@ app.get('/api/transaction/boxPlot/:token/:page', jsonParser, function(req, res){
 		res.statusCode = 400;
 		return res.send('Error 400: use of boxPlot with bad token.');
 	}
+	
+	var retorno = { 
+				list:[{transID: "trans1", 
+						userID: "user1", 
+						value: 21.0, 
+						geo: {lat:"-23", long: "-43"}, 
+						status:""},
+						{transID: "trans2", 
+						userID: "user1", 
+						value: 20.0, 
+						geo: {lat:"-23", long: "-43"}, 
+						status:""}], 
+				totalPages: 1, 
+				currentPage: 0
+				}
 
 
-	return res.json({status: "ok"});
+	return res.json(retorno);
 	
 });
+
+app.get('/api/transaction/query/:token/:dateStart/:dateEnd/:page', jsonParser, function(req, res){
+	if(req.params.token !== tokenFake) {
+		res.statusCode = 400;
+		return res.send('Error 400: use of query with bad token.');
+	}
+	
+	if(!moment(req.params.dateStart).isValid() ||
+	   !moment(req.params.dateEnd).isValid()){
+		res.statusCode = 400;
+		return res.send('Error 400: invalid date parameters.');	   
+	}
+	
+	var retorno = { 
+				list:[{transID: "trans1", 
+						userID: "user1", 
+						value: 21.0, 
+						geo: {lat:"-23", long: "-43"}, 
+						status:""},
+						{transID: "trans2", 
+						userID: "user1", 
+						value: 20.0, 
+						geo: {lat:"-23", long: "-43"}, 
+						status:""}], 
+				totalPages: 1, 
+				currentPage: 0,
+				startDate: req.params.dateStart,
+				endDate: req.params.dateEnd
+				}
+
+
+	return res.json(retorno);
+	
+});
+
 
 app.listen(process.env.PORT || 8899, '0.0.0.0');
