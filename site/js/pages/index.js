577f87ecca0e46f12844e502b88d96a503bcdbaa
Danilo Ramalho <danilovex@gmail.com>
2015-04-16 00:06:10 -0300

    Refactor no javascript da pagina index

--- site/js/pages/index.js
+++ site/js/pages/index.js
@@ -1,4 +1,10 @@
-$(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-  });
+index = function(){
+	var _init = function (){
+		// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+		$('.modal-trigger').leanModal();		
+	}
+	return {
+		init:_init
+	}
+}();
+
