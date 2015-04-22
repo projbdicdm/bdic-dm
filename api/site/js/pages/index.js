3c4a436ae95d07d2e2804514ebfdc72430186440
Danilo Ramalho <danilovex@gmail.com>
2015-04-22 01:58:48 -0300

    divisão de responsabilidades das telas e javascript

--- api/site/js/pages/index.js
+++ api/site/js/pages/index.js
@@ -1,54 +1,58 @@
-$(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-});
+index = function(){
+	var _init = function (){
+		//carrega conteudo do modal do login
+		 $.ajax({
+            type: 'GET',
+			async: false,
+            url: 'modal-login.html',
+            success: function (data) {
+				$("#modal-user-login").html(data);
+            }
+        });
+		//identifica div como modal
+		$('.modal-trigger').leanModal();		
+	}
+	var _api_user_login = function (){
+		var requestData = JSON.stringify($('#formLogin').serializeObject());
+		
+		$.ajax({
+			url: '/api/user/login',
+			type: 'POST',
+			contentType: 'application/json; charset=utf-8',
+			dataType: 'json',
+			data: requestData
+		}).done(function(data, textStatus, jqXHR) {
+			if(data.userType == "admin")
+				location.href = "http://localhost:8899/indexadmin.html";
+			else
+				location.href = "http://localhost:8899/indexclient.html";
+		}).fail(function(jqXHR, textStatus, errorThrown) {
+			$('#modal-user-login-fail').openModal();
+		});
+	}
+	var _api_user_resetpassword = function(form){
 
-function api_user_login(form){
+		var requestData = JSON.stringify($('#formResetPWS').serializeObject());
 
-    currentData = new Object();
-    currentData.login = form.find("#email").val();
-    currentData.password = form.find("#password").val();
-
-    $.ajax({
-        url: '/api/user/login',
-        type: 'POST',
-        contentType: 'application/json; charset=utf-8',
-        dataType: 'json',
-        data: JSON.stringify(currentData)
-    }).done(function(data, textStatus, jqXHR) {
-        if(data.userType == "admin")
-            location.href = "http://localhost:8899/indexadmin.html";
-        else
-            location.href = "http://localhost:8899/indexclient.html";
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-        $('#modal-user-login-fail').openModal();
-    });
-};
-
-function api_user_resetpassword(form){
-
-    currentData = new Object();
-    currentData.login = form.find("#email").val();
-
-    $.ajax({
-        url: '/api/user/resetpassword',
-        type: 'POST',
-        contentType: 'application/json; charset=utf-8',
-        dataType: 'json',
-        data: JSON.stringify(currentData)
-    }).done(function(data, textStatus, jqXHR) {
-        $('#modal-user-resetpassword-success').openModal();
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-        $('#modal-user-resetpassword-fail').openModal();
-    });
-};
-
-function api_user_changepassword(form){
-    alert("Tela sem implementação");
-};
-
-
-
-
-
-
+		$.ajax({
+			url: '/api/user/resetpassword',
+			type: 'POST',
+			contentType: 'application/json; charset=utf-8',
+			dataType: 'json',
+			data: requestData
+		}).done(function(data, textStatus, jqXHR) {
+			$('#modal-user-resetpassword-success').openModal();
+		}).fail(function(jqXHR, textStatus, errorThrown) {
+			$('#modal-user-resetpassword-fail').openModal();
+		});
+	}
+	var _api_user_changepassword = function (form){
+		alert("Tela sem implementação");
+	}
+	return {
+		init:_init,
+		api_user_login: _api_user_login,
+		api_user_resetpassword: _api_user_resetpassword,
+		api_user_changepassword: _api_user_changepassword
+	}
+}();
