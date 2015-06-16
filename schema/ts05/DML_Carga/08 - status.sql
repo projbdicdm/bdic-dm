2d924bfa4c3e4f0abca4a4fabd1e37d9238fd29d
Gabriel Moreira <gabrielspmoreira@yahoo.com.br>
2015-06-15 21:36:32 -0300

    Schema do BD atualizado para a v7

---
+++ schema/ts05/DML_Carga/08 - status.sql
@@ -0,0 +1,9 @@
+USE BDICDM;
+INSERT INTO status (sta_ds) VALUES 
+('Aguardando pagamento'),
+('Em análise'),
+('Paga'),
+('Disponível'),
+('Em disputa'),
+('Devolvida'),
+('Cancelada')
---
+++ schema/ts05/DML_Carga/12 - categoria.sql
@@ -0,0 +1,15 @@
+USE BDICDM;
+INSERT INTO categoria (cat_id,cat_nm) VALUES
+(1,'Notebook'),
+(2,'Ultrabook'),
+(3,'Apple'),
+(4,'Tablets'),
+(5,'Monitores'),
+(6,'Impressoras'),
+(7,'Memória'),
+(8,'Processadores'),
+(9,'Fontes'),
+(10,'Placas de Vídeo'),
+(11,'Placa-Mãe'),
+(12,'Disco Rídigo'),
+(13,'Gabinete')
