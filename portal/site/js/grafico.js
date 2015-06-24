47d9af35c23867008475ce96a073d1c3a794a1a7
Andre Olimpio <professor.andre.olimpio@gmail.com>
2015-06-24 15:54:57 -0300

    Criando vetor dinâmico para gerar o gráfico de estatísticas de vendas de produtos.

--- portal/site/js/grafico.js
+++ portal/site/js/grafico.js
@@ -2,10 +2,21 @@
 	google.load('visualization', '1.0', {'packages':['corechart']});
     google.setOnLoadCallback(drawChart);
 	
+	//Vetor para armazenar os dados vindos de outra página JS
+	function gerar_dados(){
+		var vet_dados = new array();
+		var tamanho = vet_dados.length;
+		var contador;
+		//Vetor dinâmico
+		for (contador = 1; contador <= tamanho; contador++){
+			vet_dados.push();
+		}
+	}
+	
 	//Inicializando o processo de criação do gráfico
     function drawChart() {
 		
-		//Criando o gráfico com valores fixos através da variável DATA
+		//Criando o gráfico através da variável DATA
         var data = new google.visualization.DataTable();
             data.addColumn('string', 'Dias da Semana');
             data.addColumn('number', 'Produto 1');
@@ -14,13 +25,13 @@
             data.addColumn('number', 'Produto 4');
             data.addColumn('number', 'Meta de Vendas');
             data.addRows([
-             ['Segunda-feira', 100, 85, 99, 66, 65],
-             ['Terça-feira', 100, 78, 55, 72, 62],
-             ['Quarta-feira', 100, 79, 85, 45, 88],
-             ['Quinta-feira', 100, 69, 85, 42, 77],
-             ['Sexta-feira', 100, 97, 93, 84, 74],
-			 ['Sábado', 100, 77, 93, 94, 84],
-			 ['Domingo', 100, 87, 85, 42, 74]
+             ['Segunda-feira', gerar_dados()],
+             ['Terça-feira', gerar_dados()],
+             ['Quarta-feira', gerar_dados()],
+             ['Quinta-feira', gerar_dados()],
+             ['Sexta-feira', gerar_dados()],
+			 ['Sábado', gerar_dados()],
+			 ['Domingo', gerar_dados()]
           ]);
      
 		//Customizando o gráfico através da variável OPTIONS
